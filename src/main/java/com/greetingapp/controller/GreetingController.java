2f838524d86b13d6843e817b744b51ce776facdc
VaibhavAgrawal11 <vaibhavagrawal1110@gmail.com>
2020-05-04 07:26:22 +0530

    ADDED: Method to put greetings.

--- src/main/java/com/greetingapp/controller/GreetingController.java
+++ src/main/java/com/greetingapp/controller/GreetingController.java
@@ -3,10 +3,7 @@ package com.greetingapp.controller;
 import com.greetingapp.model.Greeting;
 import com.greetingapp.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -27,5 +24,12 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return greetingService.greetingMessage(counter.incrementAndGet(),
                 String.format(template, name));
+    }
+
+    @PutMapping("/givegreeting")
+    public Greeting greeting(@RequestParam(value = "firstName") String firstName,
+                             @RequestParam(value = "lastName") String lastName) {
+        return greetingService.greetingMessage(counter.incrementAndGet(),
+                String.format(template, firstName + " " + lastName));
     }
 }
